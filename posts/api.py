3f311868f946616ee39d506b64b3422ee8801f1d
EasanA <easan.arulanantham@gmail.com>
2017-05-03 23:34:52 +0000

    put

--- posts/api.py
+++ posts/api.py
@@ -98,3 +98,32 @@ def posts_post():
     headers = {"Location": url_for("post_get", id=post.id)}
     return Response(data, 201, headers=headers,
                     mimetype="application/json")
+
+@app.route("/api/posts/<int:id>", methods=["PUT"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def put_post(id):
+    """ Edit a post """
+    data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not you return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    # Edit the post to the database
+    post = session.query(models.Post).get(id)
+    post.title = data["title"]
+    post.body = data["body"]
+    #newpost = post.models.Post(title=data["title"], body=data["body"])
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -275,6 +275,40 @@ class TestAPI(unittest.TestCase):
 
         data = json.loads(response.data.decode("ascii"))
         self.assertEqual(data["message"], "'body' is a required property")
+    def test_put_post(self):
+        postA = models.Post(title="Test1", body="Post with bells")
+       
+        session.add(postA)
+        session.commit()
+        
+        data = {
+            "title": "Example Post",
+            "body": "Just a test"
+        }
+        
+        response = self.client.put("/api/posts/{}".format(postA.id),
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+        
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+        self.assertEqual(urlparse(response.headers.get("Location")).path,
+                         "/api/posts/1")
+
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(data["id"], 1)
+        self.assertEqual(data["title"], "Example Post")
+        self.assertEqual(data["body"], "Just a test")
+        
+        posts = session.query(models.Post).all()
+        self.assertEqual(len(posts), 1)
+
+        post = posts[0]
+        self.assertEqual(post.title, "Example Post")
+        self.assertEqual(post.body, "Just a test")
+        
         
 if __name__ == "__main__":
     unittest.main()
